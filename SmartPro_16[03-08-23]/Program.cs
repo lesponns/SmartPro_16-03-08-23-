f1597059c000e1ede8b479498ba83b3eb2aa7c7f
lesponns <adamadakadam1903@gmail.com>
2023-08-03 18:39:15 +0300

    Add project files.

---
+++ SmartPro_16[03-08-23]/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPro_15_03_08_23_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = 0, stop = 0;
+
+            Console.Clear();
+            Console.Write("Start: ");
+            sayi = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+            Console.Write("Stop: ");
+            stop = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+            do
+            {
+                Console.WriteLine(sayi);
+                sayi++;
+            }
+            while (sayi <= stop);
+            Console.ReadKey();
+        }
+    }
+}
